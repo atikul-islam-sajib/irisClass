47cff2e81b489bebcc73c34a3f0d0bcc2b2a3648
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2023-10-05 18:44:16 +0200

    update setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,58 @@
 from setuptools import setup, find_packages
 
 setup(
-    name    = "irisClass",
-    description = "a demo python project for me to understand how to create it !!!",
-    author = "Atikul Isla Sajib",
-    author_email = "atikulislamsajib137@gmail.com",
-    packages = find_packages(),
-    install_requires = [
+    name = "irisClass",
+    version = "0.1.0",  # Update with the version number of your package
+    description = "A demo Python project for me to understand how to create it!",
+    author="Atikul Isla Sajib",
+    author_email="atikulislamsajib137@gmail.com",
+    url="https://github.com/atikul-islam-sajib/irisClass",  # Update with your project's GitHub repository URL
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.0",  # Add any required dependencies here
+        "torch>=1.0",
+        "scikit-learn>=0.0.1",
     ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="iris classification machine-learning",
+    project_urls={
+        "Bug Tracker": "https://github.com/atikul-islam-sajib/irisClass/issues",
+        "Documentation": "https://github.com/atikul-islam-sajib/irisClass/docs",
+        "Source Code": "https://github.com/atikul-islam-sajib/irisClass.git",
+    },
+    
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name    = "irisClass",
+#     description = "a demo python project for me to understand how to create it !!!",
+#     author = "Atikul Isla Sajib",
+#     author_email = "atikulislamsajib137@gmail.com",
+#     packages = find_packages(),
+#     install_requires = [
+#     ],
+# )
