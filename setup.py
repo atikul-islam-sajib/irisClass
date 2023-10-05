df6f3c9f8730880a4939df3f8e2a0e91ab9c16f9
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2023-10-05 15:14:46 +0200

    updated named

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name    ="iris_classification",
+    name    = "classification",
     version = "0.0.1",
     description = "a demo python project for me to understand how to create it !!!",
     author = "Atikul Isla Sajib",
-    author_email = "asajib142099@bscse.uiu.ac.bd",
+    author_email = "atikulislamsajib137@gmail.com",
     packages = find_packages(),
     install_requires = [
     ],
