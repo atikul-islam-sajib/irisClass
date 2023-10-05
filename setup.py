ec3bdbc50934831c9e022e4b979aa7265f82a1cb
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2023-10-05 15:07:58 +0200

    setup file is updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name    ="iris_classifier",
+    version = "0.0.1",
+    description = "a demo python project for me to understand how to create it !!!",
+    author = "Atikul Isla Sajib",
+    author_email = "asajib142099@bscse.uiu.ac.bd",
+    packages = find_packages(),
+    install_requires = [
+    ],
+)
