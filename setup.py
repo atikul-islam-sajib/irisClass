c8bc2511256b9a59da112874e5b4ccd6a18cad51
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2023-10-05 16:41:40 +0200

    change setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name    = "irisClass",
-    version = "0.0.0.1",
     description = "a demo python project for me to understand how to create it !!!",
     author = "Atikul Isla Sajib",
     author_email = "atikulislamsajib137@gmail.com",
